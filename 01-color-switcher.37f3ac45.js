e4921a5310dde09f5e0314ddf476e5d8383ab6a3
Akirget <57485036+Akirget@users.noreply.github.com>
2022-10-04 12:57:27 +0000

    Deploying to gh-pages from @ Akirget/goit-js-hw-11@28064ddb19388295f4e73c0cd44f585e06f7680c 🚀

--- 01-color-switcher.37f3ac45.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]"),n=null;e.addEventListener("click",(function(){e.setAttribute("disabled",!0),r.removeAttribute("disabled"),n=setInterval((function(){t.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),r.addEventListener("click",(function(){r.setAttribute("disabled",!0),e.removeAttribute("disabled"),clearInterval(n)}))}();
-//# sourceMappingURL=01-color-switcher.37f3ac45.js.map
